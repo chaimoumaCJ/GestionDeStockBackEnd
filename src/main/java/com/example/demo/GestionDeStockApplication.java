a333b72be04de4bee0aec07a71e3eff86d020a6f
chamacham <chaimajandoubi576@gmail.com>
2018-07-27 00:07:57 +0200

    first push of my backend App for stock Mgmnt

---
+++ src/main/java/com/example/demo/GestionDeStockApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDeStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDeStockApplication.class, args);
+	}
+}
